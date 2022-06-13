a6119765aaa1e21964d3c5ef5bc390ec232a58e6
Dhruvi <dhruvip.inexture@gmail.com>
2022-06-13 18:56:48 +0530

    hystrix dashboard

--- cloud-gateway/src/main/java/com/microservice/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/microservice/CloudGatewayApplication.java
@@ -2,15 +2,15 @@ package com.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableHystrix
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
-
 }
